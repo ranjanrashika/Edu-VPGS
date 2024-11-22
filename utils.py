1d4644a88ea21c68194b376906dfc655a757d258
[Rashika Ranjan] <[ranjanrashika@gmail.com]>
2024-11-22 10:54:22 +0530

    Util

---
+++ utils.py
@@ -0,0 +1,8 @@
+#Helper functions
+import pandas as pd
+from sklearn import datasets
+
+def preprocess_dataset(file_path)
+    """Load and preprocess dataset."""
+    datasets.dropna(inplace=True)
+    return preprocess_dataset
