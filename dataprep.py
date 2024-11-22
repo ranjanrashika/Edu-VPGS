144ded917fe80d307faa1a85f1f6b4acfc026b3a
[Rashika Ranjan] <[ranjanrashika@gmail.com]>
2024-11-22 10:12:00 +0530

    performed data cleaning

---
+++ dataprep.py
@@ -0,0 +1,13 @@
+#Data Preparation
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+df = pd.read_csv('"C:/Users/Rashika Ranjan/OneDrive/Desktop/DL-PROJECT/videos.csv"')
+print("Initial Data:")
+print(df.head())
+print("\nMissing Values:")
+print(df.isnull().sum())
+df.fillna(df.mean(), inplace=True)
+df.drop_duplicates(inplace=True)
+df.to_csv('cleaded_video.csv', index=False)
+print("\nCleande Data:")
+print(df.head())
