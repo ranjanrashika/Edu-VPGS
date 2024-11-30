e270832d72695e03a2f6204ddbdcc02db053588b
[Rashika Ranjan] <[ranjanrashika@gmail.com]>
2024-11-30 18:26:40 +0530

    training

--- trainingdata.py
+++ trainingdata.py
@@ -1,2 +1,13 @@
 import pandas as pd
-impo
+
+data = pd.read_csv("C:\Users\Rashika Ranjan\OneDrive\Desktop\DL-PROJECT\training_data.csv")
+
+description = data['Description'].tolist()
+keywords = data['Keywords'].tolist()
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+model = AutoModelForSequenceClassification.from_prretrained('bert-base-uncased', num_labels=len(set(keywords)))
+
+encodings = tokenizer(keywords, truncation=True, padding=True, max_length=128, return_tensors="pt")
+
